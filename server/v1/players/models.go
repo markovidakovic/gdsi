fdb6f09759ac31a3fa74ee688b13cce4ea41291f
Marko Vidaković <markovidakovic29@gmail.com>
2025-03-05 21:41:33 +0100

    players -> refactoring models and store

--- server/v1/players/models.go
+++ server/v1/players/models.go
@@ -11,24 +11,67 @@ import (
 )
 
 type PlayerModel struct {
-	Id               string              `json:"id"`
-	Height           *float64            `json:"height"`
-	Weight           *float64            `json:"weight"`
-	Handedness       *string             `json:"handedness"`
-	Racket           *string             `json:"racket"`
-	MatchesExpected  int                 `json:"matches_expected"`
-	MatchesPlayed    int                 `json:"matches_played"`
-	MatchesWon       int                 `json:"matches_won"`
-	MatchesScheduled int                 `json:"matches_scheduled"`
-	SeasonsPlayed    int                 `json:"seasons_played"`
-	Account          AccountModel        `json:"account"`
-	CurrentLeague    *CurrentLeagueModel `json:"current_league"`
-	CreatedAt        time.Time           `json:"created_at"`
+	Id                  string       `json:"id"`
+	Height              *float64     `json:"height"`
+	Weight              *float64     `json:"weight"`
+	Handedness          *string      `json:"handedness"`
+	Racket              *string      `json:"racket"`
+	MatchesExpected     int          `json:"matches_expected"`
+	MatchesPlayed       int          `json:"matches_played"`
+	MatchesWon          int          `json:"matches_won"`
+	MatchesScheduled    int          `json:"matches_scheduled"`
+	SeasonsPlayed       int          `json:"seasons_played"`
+	Elo                 int          `json:"elo"`
+	HighestElo          int          `json:"highest_elo"`
+	IsEloProvisional    bool         `json:"is_elo_provisional"`
+	Account             accountModel `json:"account"`
+	CurrentLeague       *leagueModel `json:"current_league"`
+	PreviousLeague      *leagueModel `json:"previous_league"`
+	PreviousLeagueRank  *int32       `json:"previous_league_rank"`
+	IsPlayingNextSeason bool         `json:"is_playing_next_season"`
+	CreatedAt           time.Time    `json:"created_at"`
+}
+
+type accountModel struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type leagueModel struct {
+	Id   string `json:"id"`
+	Tier int32  `json:"tier"`
+	Name string `json:"name"`
 }
 
 func (pm *PlayerModel) ScanRow(row pgx.Row) error {
-	var leagueId, leagueTitle sql.NullString
-	err := row.Scan(&pm.Id, &pm.Height, &pm.Weight, &pm.Handedness, &pm.Racket, &pm.MatchesExpected, &pm.MatchesPlayed, &pm.MatchesWon, &pm.MatchesScheduled, &pm.SeasonsPlayed, &pm.Account.Id, &pm.Account.Name, &leagueId, &leagueTitle, &pm.CreatedAt)
+	var currLeagueId, currLeagueName, prevLeagueId, prevLeagueName sql.NullString
+	var currLeagueTier, prevLeagueTier, prevLeagueRank sql.NullInt32
+	err := row.Scan(
+		&pm.Id,
+		&pm.Height,
+		&pm.Weight,
+		&pm.Handedness,
+		&pm.Racket,
+		&pm.MatchesExpected,
+		&pm.MatchesPlayed,
+		&pm.MatchesWon,
+		&pm.MatchesScheduled,
+		&pm.SeasonsPlayed,
+		&pm.Elo,
+		&pm.HighestElo,
+		&pm.IsEloProvisional,
+		&pm.Account.Id,
+		&pm.Account.Name,
+		&currLeagueId,
+		&currLeagueTier,
+		&currLeagueName,
+		&prevLeagueId,
+		&prevLeagueTier,
+		&prevLeagueName,
+		&prevLeagueRank,
+		&pm.IsPlayingNextSeason,
+		&pm.CreatedAt,
+	)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return failure.New("scanning player row", fmt.Errorf("%w -> %v", failure.ErrNotFound, err))
@@ -36,44 +79,91 @@ func (pm *PlayerModel) ScanRow(row pgx.Row) error {
 		return failure.New("database error scanning player row", fmt.Errorf("%w -> %v", failure.ErrInternal, err))
 	}
 
-	if !leagueId.Valid {
+	if !currLeagueId.Valid {
 		pm.CurrentLeague = nil
 	} else {
-		pm.CurrentLeague = &CurrentLeagueModel{
-			Id:    leagueId.String,
-			Title: leagueTitle.String,
+		pm.CurrentLeague = &leagueModel{
+			Id:   currLeagueId.String,
+			Tier: currLeagueTier.Int32,
+			Name: currLeagueName.String,
 		}
+	}
+	if !prevLeagueId.Valid {
+		pm.PreviousLeague = nil
+	} else {
+		pm.PreviousLeague = &leagueModel{
+			Id:   prevLeagueId.String,
+			Tier: prevLeagueRank.Int32,
+			Name: prevLeagueName.String,
+		}
+	}
+	if !prevLeagueRank.Valid {
+		pm.PreviousLeagueRank = nil
+	} else {
+		pm.PreviousLeagueRank = &prevLeagueRank.Int32
 	}
 
 	return nil
 }
 
 func (pm *PlayerModel) ScanRows(rows pgx.Rows) error {
-	var leagueId, leagueTitle sql.NullString
-	err := rows.Scan(&pm.Id, &pm.Height, &pm.Weight, &pm.Handedness, &pm.Racket, &pm.MatchesExpected, &pm.MatchesPlayed, &pm.MatchesWon, &pm.MatchesScheduled, &pm.SeasonsPlayed, &pm.Account.Id, &pm.Account.Name, &leagueId, &leagueTitle, &pm.CreatedAt)
+	var currLeagueId, currLeagueName, prevLeagueId, prevLeagueName sql.NullString
+	var currLeagueTier, prevLeagueTier, prevLeagueRank sql.NullInt32
+	err := rows.Scan(
+		&pm.Id,
+		&pm.Height,
+		&pm.Weight,
+		&pm.Handedness,
+		&pm.Racket,
+		&pm.MatchesExpected,
+		&pm.MatchesPlayed,
+		&pm.MatchesWon,
+		&pm.MatchesScheduled,
+		&pm.SeasonsPlayed,
+		&pm.Elo,
+		&pm.HighestElo,
+		&pm.IsEloProvisional,
+		&pm.Account.Id,
+		&pm.Account.Name,
+		&currLeagueId,
+		&currLeagueTier,
+		&currLeagueName,
+		&prevLeagueId,
+		&prevLeagueTier,
+		&prevLeagueName,
+		&prevLeagueRank,
+		&pm.IsPlayingNextSeason,
+		&pm.CreatedAt,
+	)
 	if err != nil {
 		return failure.New("database error scanning player rows", fmt.Errorf("%w -> %v", failure.ErrInternal, err))
 	}
 
-	if !leagueId.Valid {
+	if !currLeagueId.Valid {
 		pm.CurrentLeague = nil
 	} else {
-		pm.CurrentLeague = &CurrentLeagueModel{
-			Id:    leagueId.String,
-			Title: leagueTitle.String,
+		pm.CurrentLeague = &leagueModel{
+			Id:   currLeagueId.String,
+			Tier: currLeagueTier.Int32,
+			Name: currLeagueName.String,
 		}
 	}
+	if !prevLeagueId.Valid {
+		pm.PreviousLeague = nil
+	} else {
+		pm.PreviousLeague = &leagueModel{
+			Id:   prevLeagueId.String,
+			Tier: prevLeagueRank.Int32,
+			Name: prevLeagueName.String,
+		}
+	}
+	if !prevLeagueRank.Valid {
+		pm.PreviousLeagueRank = nil
+	} else {
+		pm.PreviousLeagueRank = &prevLeagueRank.Int32
+	}
+
 	return nil
-}
-
-type AccountModel struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
-}
-
-type CurrentLeagueModel struct {
-	Id    string `json:"id"`
-	Title string `json:"title"`
 }
 
 type UpdatePlayerRequestModel struct {
--- server/v1/players/store.go
+++ server/v1/players/store.go
@@ -28,24 +28,34 @@ var allowedSortFields = map[string]string{
 func (s *store) findPlayers(ctx context.Context, limit, offset int, sort *params.OrderBy) ([]PlayerModel, error) {
 	sql := `
 		select
-			player.id,
-			player.height,
-			player.weight,
-			player.handedness,
-			player.racket,
-			player.matches_expected,
-			player.matches_played,
-			player.matches_won,
-			player.matches_scheduled,
-			player.seasons_played,
-			account.id as account_id,
-			account.name as account_name,
-			league.id as league_id,
-			league.title as league_title,
-			player.created_at
+			player.id as player_id,
+			player.height as player_height,
+			player.weight as player_weight,
+			player.handedness as player_handedness,
+			player.racket as player_racket,
+			player.matches_expected as player_matches_expected,
+			player.matches_played as player_matches_played,
+			player.matches_won as player_matches_won,
+			player.matches_scheduled as player_matches_scheduled,
+			player.seasons_played as player.seasons_played,
+			player.elo as player_elo,
+			player.highest_elo as player_highest_elo,
+			player.is_elo_provisional as player_is_elo_provisional,
+			account.id as player_account_id,
+			account.name as player_account_name,
+			current_league.id as player_current_league_id,
+			current_league.tier as player_current_league_tier,
+			current_league.name as player_current_league_name,
+			previous_league.id as player_previous_league_id,
+			previous_league.tier as player_previous_league_tier,
+			previous_league.name as player_previous_league_name,
+			player.previous_league_rank as player_previous_league_rank,
+			player.is_playing_next_season as player_is_playing_next_season,
+			player.created_at as player_created_at
 		from player
 		join account on player.account_id = account.id
-		left join league on player.current_league_id = league.id
+		left join league current_league on player.current_league_id = current_league.id
+		left joint league previous_league on player.previous_league_id = previous_league.id
 	`
 
 	if sort != nil && sort.IsValid(allowedSortFields) {
