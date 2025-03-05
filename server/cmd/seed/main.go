450164266e1baf387244156dce4c16a92b43bd19
Marko Vidaković <markovidakovic29@gmail.com>
2025-03-05 12:42:55 +0100

    seed refactor

--- server/cmd/seed/main.go
+++ server/cmd/seed/main.go
@@ -1,168 +1,136 @@
-// seed is used one time for seeding the database with a developer account
-// format of arguments -arg1="val1" -arg2="val2"
 package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"time"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/markovidakovic/gdsi/server/config"
 	"github.com/markovidakovic/gdsi/server/db"
 	"github.com/markovidakovic/gdsi/server/sec"
 )
 
-type input struct {
-	name     string
-	email    string
-	dob      string
-	gender   string
-	phone    string
-	password string
-	role     string
+// right now the seed program will focus on seeding accounts but later we might add other resources
+// and before each resource seed we ask if the user wants to seed the resource, if not, move to the next resource
+
+type Account struct {
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	Dob         string `json:"dob"`
+	Gender      string `json:"gender"`
+	PhoneNumber string `json:"phone_number"`
+	Password    string `json:"password"`
+	Role        string `json:"role"`
 }
 
-func (i *input) parse() {
-	flag.StringVar(&i.name, "name", "Marko Vidaković", "Full name")
-	flag.StringVar(&i.email, "email", "marko.vidakovic@gmail.com", "Email")
-	flag.StringVar(&i.dob, "dob", "1995-09-04", "Date of birth")
-	flag.StringVar(&i.gender, "gender", "male", "Gender (male or female)")
-	flag.StringVar(&i.phone, "phone", "0989559516", "Phone number")
-	flag.StringVar(&i.password, "password", "string", "Password")
-	i.role = "developer"
+type Court struct {
+	Name string `json:"name"`
+}
 
+func main() {
+	var seedAccountsFile string
+	var seedCourtsFile string
+	flag.StringVar(&seedAccountsFile, "accounts-file", "./db/seed/accounts.json", "Path to seed file")
+	flag.StringVar(&seedCourtsFile, "courts-file", "./db/seed/courts.json", "Path to seed file")
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Seed developer account into the database\n")
+		fmt.Fprintf(os.Stderr, "Seed accounts and courts into the database\n")
 		fmt.Fprintf(os.Stderr, "Usage:\n")
 		flag.PrintDefaults()
 	}
-
 	flag.Parse()
+
+	ctx := context.Background()
+
+	// load config
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// connect db
+	db, err := db.Connect(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var confirm string
+	fmt.Printf("Do you want to seed accounts found at %s? (y/n): ", seedAccountsFile)
+	fmt.Scanln(&confirm)
+	if confirm == "y" {
+		// read seed file
+		b, err := os.ReadFile(seedAccountsFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		accounts := []Account{}
+		err = json.Unmarshal(b, &accounts)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = seedAccounts(ctx, db, accounts)
+		if err != nil {
+			log.Fatalf("seeding accounts: %v", err)
+		}
+
+		fmt.Println("accounts seeded!")
+	}
+
+	fmt.Printf("Do you want to seed courts found at %s? (y/n): ", seedCourtsFile)
+	fmt.Scanln(&confirm)
+	if confirm == "y" {
+		fmt.Println("seed courts")
+	}
 }
 
-func (i input) validate() error {
-	if i.name == "" {
-		return fmt.Errorf("name is required")
+func seedAccounts(ctx context.Context, db *db.Conn, data []Account) error {
+	tx, err := db.Begin(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to begin tx: %w", err)
 	}
-	if i.email == "" {
-		return fmt.Errorf("email is required")
-	} else if !sec.IsValidEmail(i.email) {
-		return fmt.Errorf("email is invalid")
-	}
-	if i.dob == "" {
-		return fmt.Errorf("dob is required")
-	} else {
-		if _, err := time.Parse("2006-01-02", i.dob); err != nil {
-			return fmt.Errorf("dob is invalid")
+	defer tx.Rollback(ctx)
+
+	accountSql := `
+		insert into account (name, email, dob, gender, phone_number, password, role)
+		values ($1, $2, $3, $4, $5, $6, $7)
+		returning id
+	`
+
+	playerSql := `
+		insert into player (account_id)
+		values ($1)
+	`
+
+	for _, v := range data {
+		enc, err := sec.EncryptPwd(v.Password)
+		if err != nil {
+			return err
+		}
+		v.Password = enc
+
+		var accountID string
+		err = tx.QueryRow(ctx, accountSql, v.Name, v.Email, v.Dob, v.Gender, v.PhoneNumber, v.Password, v.Role).Scan(&accountID)
+		if err != nil {
+			return fmt.Errorf("failed to insert account %s -> %w", v.Email, err)
+		}
+
+		_, err = tx.Exec(ctx, playerSql, accountID)
+		if err != nil {
+			return fmt.Errorf("failed to insert player for account %s -> %w", v.Email, err)
 		}
 	}
-	if i.gender == "" {
-		return fmt.Errorf("gender is required")
-	} else if i.gender != "male" && i.gender != "female" {
-		return fmt.Errorf("gender is invalid, expected male or female")
-	}
-	if i.phone == "" {
-		return fmt.Errorf("phone number is required")
-	} else if !sec.IsValidPhone(i.phone) {
-		return fmt.Errorf("phone number is invalid")
-	}
-	if i.password == "" {
-		return fmt.Errorf("password is required")
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to commit tx: %w", err)
 	}
 
 	return nil
 }
 
-func main() {
-	inp := input{}
-
-	// parse cli args
-	inp.parse()
-
-	err := inp.validate()
-	if err != nil {
-		log.Fatalf("invalid input: %v", err)
-	}
-
-	// confirmation prompt
-	fmt.Printf("about to seed developer account with email %s. continue? (y/n): ", inp.email)
-	var confirm string
-	fmt.Scanln(&confirm)
-	if confirm != "y" {
-		log.Fatal("operation aborted")
-	}
-
-	// encrypt pwd
-	inp.password, err = sec.EncryptPwd(inp.password)
-	if err != nil {
-		log.Fatalf("encrypting password: %v", err)
-	}
-
-	cfg, err := config.Load()
-	if err != nil {
-		log.Fatalf("loading config: %v", err)
-	}
-
-	db, err := db.Connect(cfg)
-	if err != nil {
-		log.Fatalf("connecting database: %v", err)
-	}
-
-	ctx := context.Background()
-
-	err = seedDeveloperAccount(ctx, db, inp)
-	if err != nil {
-		log.Fatalf("seeding developer account: %v", err)
-	}
-}
-
-func seedDeveloperAccount(ctx context.Context, db *db.Conn, in input) error {
-	// queries
-	sql1 := `
-		insert into account (name, email, dob, gender, phone_number, password, role)
-		values ($1, $2, $3, $4, $5, $6, $7)
-		returning id
-	`
-	sql2 := `
-		insert into player (account_id)
-		values ($1)
-	`
-
-	var dest string
-
-	// begin tx
-	tx, err := db.BeginTx(ctx, pgx.TxOptions{})
-	if err != nil {
-		return err
-	}
-
-	defer func() {
-		if tx != nil {
-			_ = tx.Rollback(ctx)
-		}
-		log.Println("developer account seeded")
-	}()
-
-	// account
-	err = db.QueryRow(ctx, sql1, in.name, in.email, in.dob, in.gender, in.phone, in.password, in.role).Scan(&dest)
-	if err != nil {
-		return err
-	}
-
-	// player
-	_, err = db.Exec(ctx, sql2, dest)
-	if err != nil {
-		return err
-	}
-
-	err = tx.Commit(ctx)
-	if err != nil {
-		return err
-	}
-
+func seedCourts(ctx context.Context, db *db.Conn, data []Court) error {
 	return nil
 }
--- server/db/migrations/20250110182359_create_court_table.sql
+++ server/db/migrations/20250110182359_create_court_table.sql
@@ -2,7 +2,6 @@
 create table court(
     id uuid primary key not null default uuid_generate_v4(),
     name varchar(250) not null unique,
-    creator_id uuid not null references account (id),
     created_at timestamptz not null default current_timestamp
 );
 
